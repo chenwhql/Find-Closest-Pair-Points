16d997805c2200f7a0ab5c9658dcc12a19f1373d
chenwhql <sunny_cwh@163.com>
2016-10-06 00:38:48 +0800

    直接排序找中位数，还是存在跑死了的BUG

--- find-closest-pair-of-points/find-closest-pair-of-points/main.cpp
+++ find-closest-pair-of-points/find-closest-pair-of-points/main.cpp
@@ -53,96 +53,19 @@ struct PointPair
 {
 	Point A;
 	Point B;
-	
+
 	PointPair(Point A, Point B) :A(A), B(B){};
 
 };
 /**************************************/
 
 vector<Point> point_set;  //存放输入的点集
+vector<int> point_x;  //存放输入的x坐标
 vector<PointPair> closest_pair_of_points;  //存放寻找的结果
 double min_distance = 100000;  //存放最短距离
 
-/*************select 第i小的元素*****************/
-//算法导论9.3节 SELECT算法 找出能够将点集等分的x轴
-//1. 获取中位数  注意这是数值上的中位数，用作划分，而不是划分点集的中间轴
-int get_median(vector<int>& A, int p, int r)
-{
-	vector<int> median_set; //存放各组的中位数
-	int n = r - p + 1;
-	int last_group_member_num = n % 5;
-	int five_member_group_num = n / 5; //n个元素划分为（n/5）向上取整个组
+/*************找出划分点集的轴*****************/
 
-	for (int i = 0; i < n / 5; i++)
-	{
-		sort(A.begin() + p + i * 5, A.begin() + p + i * 5 + 5);  //排序
-		median_set.push_back(A[p + i * 5 + 2]);
-	}
-	if (last_group_member_num != 0)
-	{
-		sort(A.begin() + n - last_group_member_num, A.end());
-		median_set.push_back(A[r - last_group_member_num + 1 + (last_group_member_num - 1)/2]);
-	}
-
-	//递归结束条件
-	if (median_set.size() == 1)
-	{
-		return median_set[0];
-	}
-	else
-	{
-		return get_median(median_set, 0, median_set.size()-1);
-	}
-
-	return 0; //不会执行到
-}
-
-//2. 用中位数对数组进行划分
-int partition(vector<int>& A, int p, int r, int key)
-{
-	//找出x坐标中位数对应的点的位置,并与最后一个点交换
-	for (int i = p; i <= r; ++i)
-	{
-		if (A[i] == key)
-		{
-			swap(A[i], A[r]);
-		}
-	}
-	//记录主键
-	int i = p - 1;
-	for (int j = p; j < r; j++)
-	{
-		if (A[j] <= key)
-		{
-			i++;
-			swap(A[j], A[i]);
-		}
-	}
-	swap(A[i + 1], A[r]);
-
-	return i + 1;
-}
-
-//3. 选择第i小的元素
-int select(vector<int>& A, int p, int r, int i)
-{
-	//获取数集的中位数
-	int median = get_median(A, p, r);
-	int q = partition(A, p, r, median);
-	int k = q - p + 1;
-	if (i == k)
-	{
-		return A[p + i];
-	}
-	else if (i < k)
-	{
-		select(A, p, q - 1, i);
-	}
-	else
-	{
-		select(A, q + 1, r, i - k);
-	}
-}
 /*********************************************/
 
 /***************点集划分函数********************/
@@ -239,15 +162,9 @@ double find_closest_pair_of_points(vector<Point>& P, int p, int r)
 	}
 	else  //还剩不止3个点
 	{
-		//提取点集的x坐标集
-		vector<int> point_x;
-		for (unsigned int i = p; i <= r; ++i)
-		{
-			point_x.push_back(P[i].x);
-		}
 		//获取划分点集的轴
-		int x_median = select(point_x, 0, point_x.size()-1, (point_x.size()-1)/2);
-		int divide_x = point_set_partition(P, p, r, x_median);  //有多个x坐标相等的点，并且与divide_x相等，造成了死循环
+		int x_median = point_x[p + (r - p) / 2];
+		int divide_x = point_set_partition(P, p, r, x_median);
 		//递归求解两边的最短距离
 		double min_dis_left = find_closest_pair_of_points(P, p, divide_x - 1);
 		double min_dis_right = find_closest_pair_of_points(P, divide_x + 1, r);
@@ -267,8 +184,12 @@ double find_closest_pair_of_points(vector<Point>& P, int p, int r)
 		//每一个点与其后的7个点相比
 		for (int i = 0; i < point_set_middle.size(); ++i)
 		{
-			for (int j = i + 1; j <= ((i + 7) < point_set_middle.size() ? (i + 7) : point_set_middle.size()-1); ++j)
+			for (int j = i + 1; j <= ((i + 7) < point_set_middle.size() ? (i + 7) : point_set_middle.size() - 1); ++j)
 			{
+				//如果纵向间距已经大于min_dis，可以结束该轮比较
+		/*		if (abs(point_set_middle[j].x - point_set_middle[i].x) > min_dis)
+					break;*/
+				//注意轴上的点划分
 				if ((point_set_middle[i].x < x_median && point_set_middle[j].x > x_median) ||
 					(point_set_middle[i].x > x_median && point_set_middle[j].x < x_median) ||
 					point_set_middle[i].x == x_median)
@@ -299,7 +220,7 @@ double find_closest_pair_of_points(vector<Point>& P, int p, int r)
 int main()
 {
 #ifdef LOCAL
-	freopen("in_500_000.txt","r",stdin);
+	freopen("in_500_000.txt", "r", stdin);
 #endif
 	int a, b;
 	double t_start = 0.0, t_end = 0.0;
@@ -310,13 +231,15 @@ int main()
 	//input
 	while (scanf("%d %d", &a, &b) == 2)
 	{
+		point_x.push_back(a);
 		point_set.push_back(Point(a, b));
 	}
 
 	//记录开始时间
 	t_start = (double)clock() / CLOCKS_PER_SEC;
 	//计算
-	double closest_distance = find_closest_pair_of_points(point_set, 0, point_set.size()-1);
+	sort(point_x.begin(), point_x.end());
+	double closest_distance = find_closest_pair_of_points(point_set, 0, point_set.size() - 1);
 	//记录结束时间
 	t_end = (double)clock() / CLOCKS_PER_SEC;
 	//输出计算时间
